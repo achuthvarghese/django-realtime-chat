d6b23cd9721f043e79e793241f6a6e9a70935b7f
achuthvarghese <acvknr@gmail.com>
2022-08-31 21:22:47 +0530

    Add User signup

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from account.views import user_login, user_logout
+from account.views import user_login, user_logout, user_signup
 
 app_name = "account"
 
 urlpatterns = [
     path("login/", user_login, name="login"),
     path("logout/", user_logout, name="logout"),
+    path("signup/", user_signup, name="signup"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,7 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, get_user_model, login, logout
 from django.shortcuts import redirect, render
+
+User = get_user_model()
 
 
 def user_login(request):
@@ -26,3 +28,35 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect("account:login")
+
+
+def user_signup(request):
+    template = "account/signup.html"
+    context = {
+        "title": "Signup",
+    }
+
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+
+        if password1 != password2:
+            return redirect("account:signup")
+
+        if username and email and password1 and password2:
+            user = User.objects.create_user(username, email, password1)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.save()
+            return redirect("account:login")
+        else:
+            return redirect("account:signup")
+    else:
+        if request.user.is_authenticated:
+            return redirect("chat:chat")
+        else:
+            return render(request, template, context)
